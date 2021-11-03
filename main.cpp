c2239f66e50e60a7ab56a57d2381fa5e8ce0a4d3
Keita Morisaki <kmta1236@gmail.com>
2021-11-03 13:34:05 +0000

    スニペットを追加

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    cout << "Hello World\n";
-    cout << "Input: ";
-    string data;
-    getline(cin, data);
-    cout << "Output: " << data << "\n\n";
-    return 0;
+using ll = long long;
+#define int long long
+using P = pair<int, int>;
+
+#define LOG(variable) cerr << #variable":\t" << (variable) << "\n"
+#define LOGCON(i, container) for(int (i) = 0; (i) < (container).size(); ++(i)) cerr << (i) << ":\t" << (container)[(i)] << "\n"
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPS(i, r, n) for (int i = (r); i < (n); ++i)
+#define REPR(i, n) for(int i = (n); i >= 0; --i) // from n to 0
+#define REPRS(i, n, r) for(int i = (n); i >= (r); --i) // from n to r
+#define REPOBJ(itr, obj) for(auto itr = (obj).begin(); itr != (obj).end() ; ++itr)
+#define REPROBJ(itr, obj) for(auto itr = (obj).rbegin(), e = (obj).rend(); itr != e; ++itr)
+#define COUTB(x) cout << (x) << "\n"
+#define COUTS(x) cout << (x) << " "
+#define PB push_back
+#define SORT(obj) sort((obj).begin(), (obj).end())
+#define SORTR(obj) sort((obj).begin(), (obj).end(), greater<>())
+#define ALL(obj) (obj).begin(), (obj).end()
+#define MOD 1000000007
+#define PI (acos(-1))
+template<typename T = int>
+T in() {T a; cin >> a; return a;}
+/***** MAIN *****/
+signed main() {
+  
+  cout << "\n";
+  return 0;
 }
+/***** MAIN *****/
