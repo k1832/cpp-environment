134a1ec5cd715244025773f43028f0ed4d706d44
Keita Morisaki <keyz@google.com>
2022-10-03 13:00:10 +0000

    Add random test case generator

---
+++ template/generator.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import random
+
+def get_input() -> str:
+    raise NotImplemented
+
+def get_output() -> str:
+    raise NotImplemented
+
+def main():
+    # Reserve [1, case_start-1] for AtCoder sample cases
+    case_start = 10
+    case_count = 30
+    for case_i in range(case_start, case_start + case_count + 1):
+
+        with open(f"in_{case_i}.txt", 'w') as f:
+            input_str = get_input().rstrip()
+            f.write(input_str + "\n")
+
+        with open(f"out_{case_i}.txt", 'w') as f:
+            output_str = get_output().rstrip()
+            f.write(output_str + "\n")
+
+if __name__ == '__main__':
+    main()
