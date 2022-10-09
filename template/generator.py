9d9b289a6a6ed825c0cb139bc8e67a14e2d8b68d
Keita Morisaki <keyz@google.com>
2022-10-09 14:23:25 +0800

    test generator: Generate input and output first

--- template/generator.py
+++ template/generator.py
@@ -12,13 +12,13 @@ def main():
     case_start = 10
     case_count = 30
     for case_i in range(case_start, case_start + case_count + 1):
+        input_str = get_input().rstrip()
+        output_str = get_output().rstrip()
 
         with open(f"in_{case_i}.txt", 'w') as f:
-            input_str = get_input().rstrip()
             f.write(input_str + "\n")
 
         with open(f"out_{case_i}.txt", 'w') as f:
-            output_str = get_output().rstrip()
             f.write(output_str + "\n")
 
 if __name__ == '__main__':
